6804e07e36c8ae290700d8d36fdfe820f01a85c1
onionmccabbage <onionmccabbage@live.com>
2025-06-23 10:11:50 +0100

    a simple abstract class

---
+++ my_classes/abstract_shape.py
@@ -0,0 +1,13 @@
+# ABC is an Abstract Base Class
+from abc import ABCMeta, abstractmethod
+
+class AbstractShape(metaclass=ABCMeta):
+    '''Here we declare any methods and properties we wish to be included
+    But an abstract class contaons NO implementation code'''
+    @abstractmethod # @ is a decorator
+    def num_sides(self):
+        pass # remember - we write no actual implementaion within abstract class
+    @abstractmethod
+    def __str__(self):
+        '''__str__ will be used if we ever print an instance of the class'''
+        pass
