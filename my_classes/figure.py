14f58841367c02a9bd2adaafec2768419f12c850
onionmccabbage <onionmccabbage@live.com>
2025-06-23 11:43:52 +0100

    calling super init and passing self

---
+++ my_classes/figure.py
@@ -0,0 +1,32 @@
+from concrete_shape import Shape
+
+class Figure(Shape):
+    '''All the properties and methods of 'Shape' are now avauilable inside 'Figure'''
+    # we may declare class properties
+    count = 0 # this is available to the class (not any particular instance)
+    def __init__(self, num_sides, colour='black', id=False):
+        # We usually call the __init__ of the super class
+        # super().__init__(num_sides, colour) # NB self will automatically be passed
+        # alternatively CAREFUL - we must pass 'self'
+        Shape.__init__(self, num_sides, colour)
+        self.id = id
+        Figure.count += 1 # increment the count
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, new_id):
+        '''Validate ID to be a positive integer'''
+        if type(new_id)==int and new_id >=0:
+            self.__id = new_id
+        else:
+            self.__id = Figure.count # the count belongs to the class (not to any instance)
+    def __str__(self):
+        s = super().__str__() # this gives us a nice string
+        return f'{s} id: {self.id}'
+
+if __name__ == '__main__':
+    f1 = Figure(4, 'orange') # the ID should be 0
+    f2 = Figure(7, 'red')    # the id should be 1
+    f3 = Figure(8, 'ochre')  # the id should be 2
+    print(f1, f2, f3)
