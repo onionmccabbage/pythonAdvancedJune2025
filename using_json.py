c89ea5792285f22b5b8b8607d8643961bcaf92ba
onionmccabbage <onionmccabbage@live.com>
2025-06-23 12:27:14 +0100

    using json.load to convet a loaded text file into a Python structure

--- using_json.py
+++ using_json.py
@@ -11,7 +11,21 @@ def makeStruct(text):
     s = json.loads(text)
     return s
 
+def readJSON(fn):
+    '''Read in JSON text from a specified file name'''
+    try:
+        fin = open(fn, 'rt') # we now have a file access object
+        f = json.load(fin) # NB the file must contain valid JSON
+        return f # the text will automatically be converted to a Python dict or list
+    except Exception as err:
+        print(err)
+
 if __name__ == '__main__':
+    # try to load JSON from an external file
+    result = readJSON('photos.json')
+    print(result, type(result))
+
+
     my_data = {'main':'eggs', 'side':'salad', 'after':'icecream'}
     print(my_data, type(my_data)) # a dict
     my_data_j = useJSON(my_data)
