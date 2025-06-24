e4d53ae5505a3b6ce5aed5e9cd9fdc8b5a67252b
onionmccabbage <onionmccabbage@live.com>
2025-06-24 10:13:04 +0100

    read from DB

---
+++ db/read_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def readDB():
+    '''read back all entries from our database'''
+    conn = sqlite3.connect('my_db')
+    curs = conn.cursor()
+    st   = '''
+    SELECT creature, cost, count FROM zoo
+    '''
+    try:
+        curs.execute(st)
+        conn.commit()
+        # we may have a result come back from the DB
+        rows = curs.fetchall()
+        conn.close()
+        return rows # this will be a list
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    r = readDB()
+    print(r, type(r))
