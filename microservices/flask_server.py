8e92292346bb853bbf2de05ced20d4432327525c
onionmccabbage <onionmccabbage@live.com>
2025-06-25 16:25:05 +0100

    using Flask as a microservice Web server

--- microservices/flask_server.py
+++ microservices/flask_server.py
@@ -4,7 +4,7 @@
 # we would need to install flask: python -m pip install flask
 from flask import Flask
 
-# in Python we are permitted ot declare functions inside functions...
+# in Python we are permitted to declare functions inside functions...
 def main():
     '''declare and run a Flask web server'''
     app = Flask(__name__)
@@ -12,7 +12,21 @@ def main():
     @app.route('/') # this is the root of our web server
     def root():
         return 'Welcome to this Flask server'
-    
+    @app.route('/greet')
+    def greet():
+        return('greeting')
+    # we may have common mis-spellings or alternative brand names...
+    @app.route('/aluminum')
+    @app.route('/aluminium')
+    def al():
+        return('it is spelled "aluminium"')
+    # we might use flask as a microservice proxy to other services
+    @app.route('/data')
+    # we could make DB, API or file access requests to retrieve some data
+    def data():
+        response = '<h2>data goes here</h2>'
+        return response
+
     # we need our serer to run endlessly
     app.run()
 
