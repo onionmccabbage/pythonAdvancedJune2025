b60bddf7e2f85edb01df08d8379d349ff8ae8d16
onionmccabbage <onionmccabbage@live.com>
2025-06-25 12:12:04 +0100

    using unittest

---
+++ working_with_tests/point_test.py
@@ -0,0 +1,16 @@
+# this file contains unit tests for our Point class
+import unittest
+from point import Point
+
+class testPoint(unittest.TestCase):
+    '''execute tests against the Point class'''
+    # we set up a new Point to be used for each test (no test should depend on other tests)
+    def setUp(self):
+        self.point = Point(3,4)
+    # each test then uses a new Point as declared in setUp
+    def testMoveBy(self):
+        self.point.moveBy(-5, 2)
+        self.assertEqual(self.point.display(), (-2, 6))
+
+if __name__ == '__main__':
+    unittest.main() # this will execute the tests
