6d22c3e244463fee2648078e407bdff937b9412e
onionmccabbage <onionmccabbage@live.com>
2025-06-26 12:01:11 +0100

    balancing threads and work to be done

--- using_threads/d_thread.py
+++ using_threads/d_thread.py
@@ -1,14 +1,16 @@
 import threading # or from threading import Thread
 import time
+import timeit # really good at measuring only the time taken by Python (ignore OS)
 import random
 
 testsAvailable = [] # this will end up containing 100 tests dict items
-numthreads     = 8 # we may vary how many threads we choose to use
+# careful - the OS will limit how many concurrent threads we may use
+numthreads     = 1024 # we may vary how many threads we choose to use
 
-for _ in range(0,100):
+# we often need to balance the number of threads with the number of things that need doing
+for _ in range(0,4098): 
     t = {'id':_, 'kind':'Python'}
     testsAvailable.append(t)
-
 
 class TestRunner(threading.Thread):
     '''Each instasnce will be executed on a separate thread
@@ -31,7 +33,7 @@ class TestRunner(threading.Thread):
             else: # if there are no more tests
                 running = False
             self.lock.release()
-        print(f'Ran {self.test_count}')
+        # print(f'Ran {self.test_count}')
     def randomDelay(self):
         time.sleep(random.randint(0,4)/4) # 0, 0.25, 0.5, 1.0
 
@@ -42,11 +44,11 @@ if __name__ == '__main__':
     for _ in range(0, numthreads):
         runner = TestRunner(lock)
         runners_l.append(runner)
-    s = time.time()
+    s = timeit.default_timer()
     for _ in runners_l:
         _.start()
     for _ in runners_l:
         _.join()
-    e = time.time()
+    e = timeit.default_timer()
     print(f'total {e-s} sec')
 
