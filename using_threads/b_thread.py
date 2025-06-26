6b97c0704b9f2d0639b9a29964a3448352eef513
onionmccabbage <onionmccabbage@live.com>
2025-06-26 10:26:44 +0100

    we dont have to use join

--- using_threads/b_thread.py
+++ using_threads/b_thread.py
@@ -4,31 +4,33 @@ import random
 
 class MyClass(Thread): # our class inherists from the Thread class
     '''Any class may inherit from Thread'''
-    def __init__(self, n):
+    def __init__(self, n, x):
         # if we call super().__init__ we MUST provide all the required arguments
         # super().__init__(group, target, name, args, kwargs, daemon=daemon)
         Thread.__init__(self) # this is easier
         self.n = n # we could validate with property get/set and mangled names
+        self.x = x
     # to behave as a thread we must overwrite the run method
     def run(self):
         '''When this class is invoked the run method is called'''
         for _ in range(0,4):
             print(f'{self.n} is sleeping')
-            time.sleep( random.random()*0.5 ) # sleep for 0-0.5 sec
+            time.sleep( random.random()*self.x ) # sleep for 0-0.5 sec
 
 if __name__ == '__main__':
     print('on the main thread')
-    tA = MyClass('A')
-    tB = MyClass('B')
-    tC = MyClass('C')
+    tA = MyClass('A', 0.5)
+    tB = MyClass('B', 0.5)
+    tC = MyClass('C', 2)
     start = time.time()
     tA.start()
     tB.start()
     tC.start()
     # we may pause the main thread until the other thrads complete
     tA.join()
-    tB.join()
-    tC.join()
+    # tB.join()
+    # tC.join()
+
 
     end = time.time()
     print(f'Total execution: {end-start} sec')
