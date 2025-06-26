4734de19a33e6833c3fffbee26f1209694368028
onionmccabbage <onionmccabbage@live.com>
2025-06-26 12:13:44 +0100

    using asyncio

---
+++ using_threads/e_async.py
@@ -0,0 +1,22 @@
+# NB async is not the same as threading
+import timeit
+import asyncio 
+
+# we must declare a function as 'async' in order to run is asynchronously
+async def main():
+    '''we may run any async funtion asynchronously'''
+    await asyncio.sleep(1) # here would be our long-running functionality
+    print('all done')
+
+if __name__ == '__main__':
+    # asyncio.run( main() )
+    # we may run severla instances of our async function
+    s = timeit.default_timer()
+    with asyncio.Runner() as runner:
+        runner.run(main())
+        runner.run(main())
+        runner.run(main())
+        runner.run(main())
+        runner.run(main())
+        runner.run(main())
+    print(f'total execution: {timeit.default_timer-s}')
